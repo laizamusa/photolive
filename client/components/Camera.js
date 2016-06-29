6e86872de0cf29e5ab6b608de102318ee8f336ad
roelrosil1705 <roelrosil1705@gmail.com>
2016-06-29 14:51:33 +0800

    camera good

--- client/components/Camera.js
+++ client/components/Camera.js
@@ -1,11 +1,8 @@
-var React = require('react');
+var React = require('react');   
 
 var Camera = React.createClass({
 
     getInitialState: function() {
-        // var canvas = document.querySelector('#c');
-        // var gCtx = canvas.getContext('2d');
-        
         return {
             windowWidth: window.innerWidth
         };
@@ -46,19 +43,6 @@ var Camera = React.createClass({
         v.play();
         window.requestAnimationFrame(this.draw);        
     },
-    
-    // draw: function() {
-    //     var w = canvas.clientWidth;
-    //     var h = canvas.clientHeight;
-    //     backBuffer.width = w;
-    //     backBuffer.height = h;
-    //     var context = canvas.getContext('2d');
-    //     context.drawImage(v, 0, 0, 640, 480);
-    //     // bCtx.drawImage(v, 0, 0, w, h);
-    //     // pixelOperationFunction(w,h);
-    //     window.requestAnimationFrame(draw);
-    //
-    // },
 
     render: function() {
         return (
@@ -68,8 +52,9 @@ var Camera = React.createClass({
             //     <canvas id = "c"/>
             React.createElement('div', {id: 'camera'},
                 React.createElement('div', {className: 'content-page'},
-                    React.createElement('video', {id: 'v', width: 640, height: 480}),
-                    React.createElement('canvas', {id: 'c', width: 640, height: 480, style: {display: 'none'}})
+                    React.createElement('video', {id: 'v', width: 960, height: 720}),
+                    React.createElement('canvas', {id: 'c', width: 960, height: 720, style: {display: 'block'}}),
+                    React.createElement('canvas', {id: 'mainframe', width: 960, height: 720, style: {display: 'block'}, src:'../../image/sample.png'})
                 ),
                 <div><Button/></div>
             )
@@ -82,40 +67,38 @@ var Camera = React.createClass({
 var Button = React.createClass({
 
     render: function() {
-        // var context = canvas.getContext('2d');
         var label = 'Button';
-        // alert('hello');
         return (
             React.createElement('div', {className: 'footer-btn'},
-                // React.createElement('button', {className: 'button one-button', onclick:'alert("hello")'}, label)
                 <button class="button one-button" onClick={this.handleThatEvent}>Snap</button>
             )
         )
 
     },
     handleThatEvent:function (e) {
+        var MainFrame = '../../image/sample.png';
         var v = document.querySelector('#v');
         var canvas = document.querySelector('#c');
         var w = canvas.clientWidth;
         var h = canvas.clientHeight;
         var context = canvas.getContext('2d');
-        context.drawImage(v, 0, 0, 640, 480);
 
-        var head = /^data:image\/(png|jpeg);base64,/
-        var data = canvas.toDataURL('image/jpeg', 1.0).replace(head, '');
-
-        $.ajax
-        ({
-            url: dest_url,
-            type: 'post',
-            cache: false,
-            success: function()
-            {
-                loadPage4();
-            }
-
-        });
-
+        // context.drawImage(v, 0, 0, w, h);
+        // var head = /^data:image\/(png|jpeg);base64,/
+        // var data = canvas.toDataURL('image/jpeg', 1.0).replace(head, '');
+        //
+        // var imageObj2 = new Image();
+        // imageObj2.onload = function (){
+        //     context.drawImage(imageObj2, 0, 0, w, h);
+            // var imageObj1 = new Image();
+            // imageObj1.onload = function (){
+            //     // imageObj1.style.objectFit = "cover";
+            //     context.drawImage(imageObj1, 0, 0, w, h);
+            // };
+            // imageObj1.src = canvas.toDataURL();
+        // };
+        // console.log(location.origin+"/photolive/image/sample.png");
+        // imageObj2.src = MainFrame;
     }
 
 });
